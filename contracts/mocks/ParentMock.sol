faee948cd22e7654bc5af3d314f0dbe31a98c515
Bohdan Pukhno <pukhnobg@gmail.com>
2024-06-19 14:18:04 +0300

    remove: redundant files

--- contracts/mocks/ParentMock.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.19;
-import "../Parent.sol";
-
-contract ParentMock is  Parent{
-    constructor(IManager _manager) Parent(_manager) {
-
-    }
-}
